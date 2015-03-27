942e5cb88b36bfd5ef127dd8d1de45ef86062e16
Mikko Hellsing <mikko@aino.se>
2015-03-27 10:19:23 +0100

    2

--- products_brand/models.py
+++ products_brand/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-from products.models import Product
+from sellout.base import extend_model
 
 
-class Product(Product):
-    brand = models.CharField(max_length=500)
+class Brand(models.Model):
+    name = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return self.name
+
+
+class ProductMixin(object):
+    brand = models.ForeignKey(Brand)
 
     def __unicode__(self):
         return '%s -- %s' % (self.title, self.brand)
+
+
+extend_model('products.Product', ProductMixin)
